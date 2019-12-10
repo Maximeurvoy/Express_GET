0111537ce70f05ac58c66455ca2ca2452df29a54
Maximeurvoy <maxime.ptitx@gmail.com>
2019-12-10 11:57:22 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const connection = require('./conf');
 
 const app = express();
 const port = 3000;
 
+app.get('/api/movie/:id', (req, res) => {
+  connection.query(`SELECT * FROM movie WHERE id = ${mysql.escape(req.params.id)}`, (err, results) => {
+    if (err) {
+      res.status(500).send(`ben ça marche po :${err.message}`);
+    } else if (results.length <= 0) {
+      res.status(404).send(`Movie not found`);
+    } else {
+      res.send(results);
+    }
+  });
+});
+ 
 // respond to requests on `/api/employees`
-app.get('/api/employees', (req, res) => {
+app.get('/api/movie', (req, res) => {
+  let sql = 'SELECT * FROM movie';
+  const sqlValues = [];
+  if (req.query.rating) {
+    sql += ' WHERE rating = ?';
+    sqlValues.push(req.query.rating);
+  }
+  if (req.query.genre) {
+    sql += ' WHERE genre = ?';
+    sqlValues.push(req.query.genre);
+  }
   // send an SQL query to get all employees
-  connection.query('SELECT * FROM employee', (err, results) => {
+  connection.query(sql, sqlValues, (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
       res.status(500).send(`An error occurred: ${err.message}`);
